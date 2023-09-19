6a0df542bd7f8cbdf765425c62d66c18edbb3b1d
daianasantosferreira <125597466+daianasantosferreira@users.noreply.github.com>
2023-09-19 12:46:02 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.endDaiana Aparecida Ferreira dos Santos!');
+}).listen8014);var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hel
